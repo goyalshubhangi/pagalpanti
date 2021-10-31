23d292877456b8815f0a8b8bafd6eae12b357dec
Shubhangi <goyalshubhangi72@gmail.com>
2021-10-31 22:18:45 +0530

    todo app login signup dashboard start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import Login from "./components/Login";
+import Todo from "./components/Todo";
+import NotFound from "./components/NotFound";
+import Signup from "./components/Signup";
 
 function App() {
+  const theme = createTheme({
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+        `,
+      },
+    },
+    palette: {
+      primary: {
+        main: "#8684f3",
+        dark: "#6962e6",
+        light: "#7367F0",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/signup" exact>
+            <Signup />
+          </Route>
+          <Route path="/todo" exact>
+            <Todo />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
